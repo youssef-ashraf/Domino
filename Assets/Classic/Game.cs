fb53193195b6f092f5454d60adfc8c5d58f9e00d
youssef-ashraf <61710199+youssef-ashraf@users.noreply.github.com>
2020-10-15 15:16:14 +0200

    ..

--- Assets/Classic/Game.cs
+++ Assets/Classic/Game.cs
@@ -14,14 +14,18 @@ public class Game : MonoBehaviour
     Piece[] Hand3 = new Piece[7];
     Piece[] Hand4 = new Piece[7];
 
+    GameObject[] Hand2_rand = new GameObject[7];
+    GameObject[] Hand3_rand = new GameObject[7];
+    GameObject[] Hand4_rand = new GameObject[7];
+
 
     // Start is called before the first frame update
     void Start()
     {
         float start_pos = -2.88f;
-
+        int i = 0;
         int count = 27;
-        for (int i = 6; i >= 0; i--)
+        for ( i = 6; i >= 0; i--)
             for (int j = i; j >= 0; j--,count--)
             {
                 Pieces[count] = new Piece();
@@ -36,21 +40,21 @@ public class Game : MonoBehaviour
         Pieces = Pieces.OrderBy(x => rnd.Next()).ToArray();
 
 
-
+        
         //Distribution
-        for (int i = 0; i <7; i++)
+        for ( i = 0; i <7; i++)
         {
             Hand1[i] = Pieces[i];
         }
-        for (int i = 7; i < 14; i++)
+        for ( i = 7; i < 14; i++)
         {
             Hand2[i-7] = Pieces[i];
         }
-        for (int i = 14; i < 21; i++)
+        for ( i = 14; i < 21; i++)
         {
             Hand3[i-14] = Pieces[i];
         }
-        for (int i = 21; i < 28; i++)
+        for ( i = 21; i < 28; i++)
         {
             Hand4[i-21] = Pieces[i];
         }
@@ -65,31 +69,40 @@ public class Game : MonoBehaviour
         }
         start_pos= -2.88f;
 
+
+         i = 0;
         //Visualisation For Hand2
         foreach (Piece x in Hand2)
+        {   
+            var g = GameObject.Find("random");
+            Hand2_rand[i] = GameObject.Instantiate(g, new Vector3(start_pos, 3.9f, g.transform.position.z), Quaternion.identity);
+            start_pos = start_pos + 0.96f;
+            i++;
+        }
+        start_pos = -2.88f;
+
+
+        i = 0;
+        //Visualisation For Hand3
+        foreach (Piece x in Hand3)
         {
             var g = GameObject.Find("random");
-            GameObject.Instantiate(g, new Vector3(start_pos, 3.9f, g.transform.position.z),Quaternion.identity);
+            Hand3_rand[i] = GameObject.Instantiate(g, new Vector3(-7.8f, start_pos, g.transform.position.z), Quaternion.Euler(0,0,90));
             start_pos = start_pos + 0.96f;
+            i++;
         }
+        start_pos = -2.88f;
 
 
-        ////Visualisation For Hand3
-        //foreach (Piece x in Hand3)
-        //{
-        //    var g = GameObject.Find(x.big.ToString() + '_' + x.small.ToString());
-        //    g.transform.position = new Vector3(start_pos, -3.9f, g.transform.position.z);
-        //    start_pos = start_pos + 0.96f;
-        //}
-
-
-        ////Visualisation For Hand4
-        //foreach (Piece x in Hand4)
-        //{
-        //    var g = GameObject.Find(x.big.ToString() + '_' + x.small.ToString());
-        //    g.transform.position = new Vector3(start_pos, -3.9f, g.transform.position.z);
-        //    start_pos = start_pos + 0.96f;
-        //}
+        i = 0;
+        //Visualisation For Hand4
+        foreach (Piece x in Hand4)
+        {
+            var g = GameObject.Find("random");
+            Hand4_rand[i] = GameObject.Instantiate(g, new Vector3(7.8f, start_pos, g.transform.position.z), Quaternion.Euler(0, 0, 90));
+            start_pos = start_pos + 0.96f;
+            i++;
+        }
 
 
     }
